0c2fa0c32dfb28948bb0bed2d2d6198e9a3bd7c8
Akshaysaini5381 <akshaysaini5381@gmail.com>
2022-07-20 23:52:45 +0530

    Add project files.

---
+++ Collection SortedDictionary/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+namespace Collection_SortedDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SortedDictionary<int, int> s = new SortedDictionary<int, int>();
+            s.Add(1, 12);
+            s.Add(2, 12);
+            s.Add(3, 128);
+            s.Add(4, 125);
+            s.Add(5, 124);
+
+            foreach (KeyValuePair<int,int> item in s)
+            {
+                Console.WriteLine(" "+item.Key+" "+item.Value);
+            }
+            Console.WriteLine(s.Count);
+        }
+    }
+}
